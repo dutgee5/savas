16ba4a04ce3a53aae1aa66f0a05b406e37e93588
Muzaffer Döne <muzodone5@gmail.com>
2021-05-26 21:20:12 +0300

    Oyunu başlat ve bitir

--- Savas.Desktop/AnaForm.cs
+++ Savas.Desktop/AnaForm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Savas.Library.Concrete;
+using Savas.Library.Enum;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,30 @@ namespace Savas.Desktop
 {
     public partial class AnaForm : Form
     {
+        private readonly Oyun _oyun = new Oyun();
+
         public AnaForm()
         {
             InitializeComponent();
+        }
+
+        private void AnaForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Enter:
+                    _oyun.Baslat();
+                    break;
+                case Keys.Right:
+                    _oyun.UcaksavariHareketEttir(Yon.Saga);
+                    break;
+                case Keys.Left:
+                    _oyun.UcaksavariHareketEttir(Yon.Sola);
+                    break;
+                case Keys.Space:
+                    _oyun.AtesEt();
+                    break;
+            }
         }
     }
 }
---
+++ Savas.Library/Concrete/Oyun.cs
@@ -0,0 +1,45 @@
+﻿using Savas.Library.Enum;
+using Savas.Library.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Savas.Library.Concrete
+{
+    public class Oyun : IOyun
+    {
+        public bool DevamEdiyorMu { get; private set; }
+
+        public TimeSpan GecenSure { get; }
+
+        public void AtesEt()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Baslat()
+        {
+            if (!DevamEdiyorMu) return;
+           
+            DevamEdiyorMu = true;
+         
+        }
+
+        private void Bitir()
+        {
+            if (!DevamEdiyorMu) return;
+
+            DevamEdiyorMu = false;
+           
+        }
+
+
+        public void UcaksavariHareketEttir(Yon yon)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
